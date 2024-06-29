382ee109e8f89831769ffb208fe91da78be4ecbf
mahesh.k_grt <mahesh.k@grtship.com>
2024-06-29 11:15:09 +0530

    discoveryfeign0

---
+++ src/main/java/com/serviceregistry/ServiceRegisterApplication.java
@@ -0,0 +1,18 @@
+package com.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisterApplication.class, args);
+	}
+
+}
